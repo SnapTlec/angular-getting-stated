bb48c39870ab0480de44d4a27618c961e58ac146
Leonardo Brito de Souza <leonardobrito371@gmail.com>
2024-03-25 20:24:16 -0300

    adição das rotas no projeto

--- memoteca/src/app/app-routing.module.ts
+++ memoteca/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "",
+    redirectTo: "listarPensamentos",
+    pathMatch: "full"
+  },
+  {
+    path: "criarPensamento",
+    component: CriarPensamentosComponent
+  },
+  {
+    path: "listarPensamentos",
+    component: ListarPensamentosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
