362c65f7f3c3902aebf9e04e9bf4bb6b49776ef6
Leonardo Brito de Souza <leonardobrito371@gmail.com>
2024-03-25 21:19:01 -0300

    determinar a rederização do componente pensamento.

--- memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from './interface/pensamento';
 
 @Component({
   selector: 'app-pensamento',
@@ -6,11 +7,18 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./pensamento.component.css']
 })
 export class PensamentoComponent implements OnInit {
-  @Input() pensamento = {
+  @Input() pensamento : Pensamento = {
     id: "1",
     conteudo : "Aprendendo Angular",
     autoria : "Dev",
     modelo : "modelo1"
+  }
+
+  larguraPensamento() : string{
+    if(this.pensamento.conteudo.length > 256){
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
   }
   
   constructor() { }
